9ac4305feb0b4869cc8016bc5fb28a7597484b0b
Rechierli Simões <rechierliwork14@outlook.com>
2022-10-18 16:14:44 -0300

    add base_templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 # from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
